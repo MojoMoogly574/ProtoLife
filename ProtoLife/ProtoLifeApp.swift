12cc30382d3882224c1db0087636937806ab74fd
Joseph DeWeese <rangerboy901@icloud.com>
2023-06-01 10:39:54 -0500

    Initial Commit

---
+++ ProtoLife/ProtoLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoLifeApp.swift
+//  ProtoLife
+//
+//  Created by Joseph DeWeese on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ProtoLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
